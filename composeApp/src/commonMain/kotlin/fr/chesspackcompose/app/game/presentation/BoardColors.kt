aa07a64c631c5b762dff669af6bacc41b80ab2b5
François Legrand <francois_legrand@ext.connect-tech.sncf>
2025-03-18 00:53:58 +0100

    Fix colors

--- composeApp/src/commonMain/kotlin/fr/chesspackcompose/app/game/presentation/BoardColors.kt
+++ composeApp/src/commonMain/kotlin/fr/chesspackcompose/app/game/presentation/BoardColors.kt
@@ -4,5 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val lightColor = Color(red = 154, green = 90, blue = 76)
 val darkColor = Color(red = 227, green = 187, blue = 178)
-val originalPositionColor = Color(red = 110, green = 219, blue = 255)
+val originalPositionColor = Color(red = 177, green = 232, blue = 153)
 val checkmatedColor = Color(red = 222, green = 102, blue = 89)
