d0353c04b7a3b5cfd4ef618b97e9f2a2af1b6266
François Legrand <francois_legrand@ext.connect-tech.sncf>
2025-03-17 19:05:15 +0100

    First commit

---
+++ composeApp/src/desktopMain/kotlin/fr/chesspackcompose/app/main.kt
@@ -0,0 +1,13 @@
+package fr.chesspackcompose.app
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Chesspack Compose",
+    ) {
+        App()
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/fr/chesspackcompose/app/MainViewController.kt
@@ -0,0 +1,5 @@
+package fr.chesspackcompose.app
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
