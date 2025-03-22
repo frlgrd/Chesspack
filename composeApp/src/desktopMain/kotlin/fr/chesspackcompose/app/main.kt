99c27fb275e243904ef8b1f6c5d04e5a0dcefef3
François Legrand <francois_legrand@ext.connect-tech.sncf>
2025-03-22 13:37:48 +0100

    Play sound for iOS

--- composeApp/src/desktopMain/kotlin/fr/chesspackcompose/app/main.kt
+++ composeApp/src/desktopMain/kotlin/fr/chesspackcompose/app/main.kt
@@ -1,14 +1,11 @@
 package fr.chesspackcompose.app
 
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import fr.chesspackcompose.app.core.di.initKoin
 
 fun main() = application {
-    LaunchedEffect(Unit) {
-        initKoin()
-    }
+    initKoin()
     Window(
         onCloseRequest = ::exitApplication,
         title = "Chesspack Compose",
--- composeApp/src/iosMain/kotlin/fr/chesspackcompose/app/core/audio/SoundEffectPlayerImpl.ios.kt
+++ composeApp/src/iosMain/kotlin/fr/chesspackcompose/app/core/audio/SoundEffectPlayerImpl.ios.kt
@@ -1,11 +1,22 @@
 package fr.chesspackcompose.app.core.audio
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.AVFAudio.AVAudioPlayer
+import platform.Foundation.NSURL
+
 actual class SoundEffectPlayerImpl : SoundEffectPlayer {
+
+    private var avAudioPlayer: AVAudioPlayer? = null
+
+    @OptIn(ExperimentalForeignApi::class)
     override fun play(uri: String) {
-        TODO("Not yet implemented")
+        val url = NSURL.URLWithString(URLString = uri) ?: return
+        avAudioPlayer = AVAudioPlayer(url, error = null)
+        avAudioPlayer?.prepareToPlay()
+        avAudioPlayer?.play()
     }
 
     override fun release() {
-        TODO("Not yet implemented")
+        avAudioPlayer?.stop()
     }
 }
