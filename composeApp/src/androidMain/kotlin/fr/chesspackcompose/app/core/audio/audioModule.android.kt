42265b5bff1bbd761f055a3c752f4af799d3be84
François Legrand <francois_legrand@ext.connect-tech.sncf>
2025-03-25 23:52:51 +0100

    inlining

--- composeApp/src/androidMain/kotlin/fr/chesspackcompose/app/core/audio/audioModule.android.kt
+++ composeApp/src/androidMain/kotlin/fr/chesspackcompose/app/core/audio/audioModule.android.kt
@@ -7,10 +7,9 @@ import org.koin.dsl.module
 actual val audioModule: Module
     get() = module {
         single<ExoPlayer> {
-            ExoPlayer.Builder(get())
-                .build().apply {
-                    playWhenReady = true
-                }
+            ExoPlayer.Builder(get()).build().apply {
+                playWhenReady = true
+            }
         }
         single<SoundEffectPlayer> { SoundEffectPlayerImpl(player = get()) }
     }
