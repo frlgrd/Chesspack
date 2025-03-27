c76031f5e54a7823ff7891eac6032f41f04e5f98
François Legrand <francois_legrand@ext.connect-tech.sncf>
2025-03-27 02:04:32 +0100

    memory optimization

--- composeApp/src/commonMain/kotlin/fr/chesspackcompose/app/game/data/BoardImpl.kt
+++ composeApp/src/commonMain/kotlin/fr/chesspackcompose/app/game/data/BoardImpl.kt
@@ -290,74 +290,99 @@ class BoardImpl(
         pieces: Set<Piece>,
         piece: Piece,
         enPassant: Pawn?,
-    ): List<PiecePosition> = when (piece) {
-        is Bishop -> diagonalMoves(pieces = pieces, piece = piece)
-        is King -> kingMoves(pieces = pieces, king = piece)
-        is Knight -> knightMoves(pieces = pieces, piece = piece)
-        is Pawn -> pawnMoves(pieces = pieces, pawn = piece, enPassant = enPassant)
-        is Queen -> queenMoves(pieces = pieces, piece = piece)
-        is Rook -> straightMoves(pieces = pieces, piece = piece)
+    ): List<PiecePosition> {
+        val pseudoLegalMoves = mutableListOf<PiecePosition>()
+        when (piece) {
+            is Bishop -> diagonalMoves(pieces = pieces, piece = piece, moves = pseudoLegalMoves)
+            is King -> kingMoves(pieces = pieces, king = piece, moves = pseudoLegalMoves)
+            is Knight -> knightMoves(pieces = pieces, piece = piece, moves = pseudoLegalMoves)
+            is Pawn -> pawnMoves(
+                pieces = pieces,
+                pawn = piece,
+                moves = pseudoLegalMoves,
+                enPassant = enPassant
+            )
+
+            is Queen -> queenMoves(pieces = pieces, piece = piece, moves = pseudoLegalMoves)
+            is Rook -> straightMoves(pieces = pieces, piece = piece, moves = pseudoLegalMoves)
+        }
+        return pseudoLegalMoves.distinct()
     }
 
     private fun queenMoves(
         pieces: Set<Piece>,
-        piece: Piece
-    ): List<PiecePosition> {
-        return straightMoves(pieces = pieces, piece = piece) +
-                diagonalMoves(pieces = pieces, piece = piece)
+        piece: Piece,
+        moves: MutableList<PiecePosition>
+    ) {
+        straightMoves(pieces = pieces, piece = piece, moves = moves)
+        diagonalMoves(pieces = pieces, piece = piece, moves = moves)
     }
 
     private fun kingMoves(
         pieces: Set<Piece>,
-        king: King
-    ): List<PiecePosition> {
-        return diagonalMoves(pieces = pieces, piece = king, max = 1) +
-                straightMoves(pieces = pieces, piece = king, max = 1) +
-                castlingMoves(pieces = pieces, king = king)
+        king: King,
+        moves: MutableList<PiecePosition>
+    ) {
+        diagonalMoves(pieces = pieces, piece = king, max = 1, moves = moves)
+        straightMoves(pieces = pieces, piece = king, max = 1, moves = moves)
+        castlingMoves(pieces = pieces, king = king, moves = moves)
     }
 
     private fun knightMoves(
         pieces: Set<Piece>,
-        piece: Piece
-    ): List<PiecePosition> = listOfNotNull(
-        jumpOverMove(pieces = pieces, piece = piece, xDirection = -1, yDirection = -2),
-        jumpOverMove(pieces = pieces, piece = piece, xDirection = 1, yDirection = -2),
-        jumpOverMove(pieces = pieces, piece = piece, xDirection = -1, yDirection = 2),
-        jumpOverMove(pieces = pieces, piece = piece, xDirection = 1, yDirection = 2),
-        jumpOverMove(pieces = pieces, piece = piece, xDirection = -2, yDirection = -1),
-        jumpOverMove(pieces = pieces, piece = piece, xDirection = 2, yDirection = -1),
-        jumpOverMove(pieces = pieces, piece = piece, xDirection = -2, yDirection = 1),
-        jumpOverMove(pieces = pieces, piece = piece, xDirection = 2, yDirection = 1),
-    )
+        piece: Piece,
+        moves: MutableList<PiecePosition>,
+    ) {
+        moves.addAll(
+            listOfNotNull(
+                jumpOverMove(pieces = pieces, piece = piece, xDirection = -1, yDirection = -2),
+                jumpOverMove(pieces = pieces, piece = piece, xDirection = 1, yDirection = -2),
+                jumpOverMove(pieces = pieces, piece = piece, xDirection = -1, yDirection = 2),
+                jumpOverMove(pieces = pieces, piece = piece, xDirection = 1, yDirection = 2),
+                jumpOverMove(pieces = pieces, piece = piece, xDirection = -2, yDirection = -1),
+                jumpOverMove(pieces = pieces, piece = piece, xDirection = 2, yDirection = -1),
+                jumpOverMove(pieces = pieces, piece = piece, xDirection = -2, yDirection = 1),
+                jumpOverMove(pieces = pieces, piece = piece, xDirection = 2, yDirection = 1),
+            )
+        )
+    }
 
     private fun straightMoves(
         pieces: Set<Piece>,
         piece: Piece,
+        moves: MutableList<PiecePosition>,
         max: Int = 7
-    ): List<PiecePosition> {
-        return searchMove(
+    ) {
+        searchMove(
             pieces = pieces,
             piece = piece,
             xDirection = -1,
             yDirection = 0,
+            moves = moves,
             max = max
-        ) + searchMove(
+        )
+        searchMove(
             pieces = pieces,
             piece = piece,
             xDirection = 0,
             yDirection = -1,
+            moves = moves,
             max = max
-        ) + searchMove(
+        )
+        searchMove(
             pieces = pieces,
             piece = piece,
             xDirection = 1,
             yDirection = 0,
+            moves = moves,
             max = max
-        ) + searchMove(
+        )
+        searchMove(
             pieces = pieces,
             piece = piece,
             xDirection = 0,
             yDirection = 1,
+            moves = moves,
             max = max
         )
     }
@@ -365,31 +390,39 @@ class BoardImpl(
     private fun diagonalMoves(
         pieces: Set<Piece>,
         piece: Piece,
+        moves: MutableList<PiecePosition>,
         max: Int = 7
-    ): List<PiecePosition> {
-        return searchMove(
+    ) {
+        searchMove(
             pieces = pieces,
             piece = piece,
             xDirection = 1,
             yDirection = 1,
+            moves = moves,
             max = max
-        ) + searchMove(
+        )
+        searchMove(
             pieces = pieces,
             piece = piece,
             xDirection = -1,
             yDirection = 1,
+            moves = moves,
             max = max
-        ) + searchMove(
+        )
+        searchMove(
             pieces = pieces,
             piece = piece,
             xDirection = -1,
             yDirection = -1,
+            moves = moves,
             max = max
-        ) + searchMove(
+        )
+        searchMove(
             pieces = pieces,
             piece = piece,
             xDirection = 1,
             yDirection = -1,
+            moves = moves,
             max = max
         )
     }
@@ -411,70 +444,77 @@ class BoardImpl(
     private fun pawnMoves(
         pieces: Set<Piece>,
         pawn: Pawn,
+        moves: MutableList<PiecePosition>,
         enPassant: Pawn?
-    ): List<PiecePosition> {
+    ) {
         val direction = if (pawn.color == PieceColor.White) -1 else 1
-        return searchMove(
+        searchMove(
             pieces = pieces,
             piece = pawn,
             xDirection = 0,
             yDirection = direction,
+            moves = moves,
             max = if (pawn.moved) 1 else 2,
             canAttackFromFront = false
-        ) + pawnAttackMoves(
+        )
+        pawnAttackMoves(
             pieces = pieces,
             piece = pawn,
-            direction = direction
-        ) + pawnEnPassantAttack(
+            direction = direction,
+            moves = moves
+        )
+        pawnEnPassantAttack(
             piece = pawn,
             enPassant = enPassant,
-            direction = direction
+            direction = direction,
+            moves = moves
         )
     }
 
     private fun pawnAttackMoves(
         pieces: Set<Piece>,
         piece: Piece,
-        direction: Int
-    ): List<PiecePosition> {
-        val attackMoves = mutableListOf<PiecePosition>()
+        direction: Int,
+        moves: MutableList<PiecePosition>,
+    ) {
         val leftPiece =
             pieceAt(pieces = pieces, x = piece.position.x - 1, y = piece.position.y + direction)
         if (leftPiece != null && leftPiece.color != piece.color) {
-            attackMoves.add(leftPiece.position)
+            moves.add(leftPiece.position)
         }
         val rightPiece =
             pieceAt(pieces = pieces, x = piece.position.x + 1, y = piece.position.y + direction)
         if (rightPiece != null && rightPiece.color != piece.color) {
-            attackMoves.add(rightPiece.position)
+            moves.add(rightPiece.position)
         }
-        return attackMoves
     }
 
     private fun pawnEnPassantAttack(
         piece: Piece,
         enPassant: Pawn?,
         direction: Int,
-    ): List<PiecePosition> {
-        enPassant ?: return emptyList()
-        if (enPassant == piece) return emptyList()
-        if (enPassant.color == piece.color) return emptyList()
-        if (enPassant.position.y != piece.position.y) return emptyList()
+        moves: MutableList<PiecePosition>
+    ) {
+        enPassant ?: return
+        if (enPassant == piece) return
+        if (enPassant.color == piece.color) return
+        if (enPassant.position.y != piece.position.y) return
         val distance = abs(enPassant.position.x - piece.position.x)
-        if (distance != 1) return emptyList()
-        return listOf(PiecePosition(x = enPassant.position.x, y = piece.position.y + direction))
+        if (distance != 1) return
+        moves.add(PiecePosition(x = enPassant.position.x, y = piece.position.y + direction))
     }
 
     private fun castlingMoves(
         pieces: Set<Piece>,
-        king: King
-    ): List<PiecePosition> {
-        return pieces
+        king: King,
+        moves: MutableList<PiecePosition>
+    ) {
+        moves.addAll(pieces
             .filterIsInstance<Rook>()
             .filter { it.color == king.color }
-            .mapNotNull { rook ->
-                if (canCastling(pieces = pieces, king = king, rook = rook)) rook.position else null
-            }
+            .filter { rook -> canCastling(pieces = pieces, king = king, rook = rook) }
+            .map { it.position }
+        )
     }
 
     private fun searchMove(
@@ -482,10 +522,10 @@ class BoardImpl(
         piece: Piece,
         xDirection: Int,
         yDirection: Int,
+        moves: MutableList<PiecePosition>,
         max: Int,
         canAttackFromFront: Boolean = true
-    ): List<PiecePosition> {
-        val moves = mutableListOf<PiecePosition>()
+    ) {
         var x = piece.position.x
         var y = piece.position.y
         var move = 0
@@ -500,7 +540,6 @@ class BoardImpl(
             moves.add(PiecePosition(x = x, y = y))
             move++
         } while (!enemyMet && move < max)
-        return moves
     }
 
     private fun pieceAt(pieces: Set<Piece>, x: Int, y: Int): Piece? {
