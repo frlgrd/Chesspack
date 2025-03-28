1074f108fb3865728ff9ccedbabb2f0c0a49f5ff
François Legrand <francois_legrand@ext.connect-tech.sncf>
2025-03-28 16:57:40 +0100

    Reset steps

--- composeApp/src/commonMain/kotlin/fr/chesspackcompose/app/game/data/CountdownTimerImpl.kt
+++ composeApp/src/commonMain/kotlin/fr/chesspackcompose/app/game/data/CountdownTimerImpl.kt
@@ -28,6 +28,7 @@ class CountdownTimerImpl : CountdownTimer {
     override fun init(duration: Duration) {
         _timeLeft.value = duration.inWholeMilliseconds
         finished = false
+        step = 1000L
         if (timerJob != null) timerJob?.cancel()
         timerJob = CoroutineScope(Dispatchers.Default).launch { while (!finished) tick() }
     }
