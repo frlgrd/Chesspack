1081d0c0a5cf4e4fd22c271a0be9b66ff81eef13
François Legrand <francois_legrand@ext.connect-tech.sncf>
2025-04-01 10:21:29 +0200

    Rename logger

---
+++ composeApp/src/wasmJsMain/kotlin/fr/chesspackcompose/app/core/logger/Logger.wasmJs.kt
@@ -0,0 +1,13 @@
+package fr.chesspackcompose.app.core.logger
+
+actual object Logger {
+    actual fun e(message: String, tag: String, throwable: Throwable?) {
+    }
+
+    actual fun d(message: String, tag: String) {
+    }
+
+    actual fun i(message: String, tag: String) {
+    }
+
+}
